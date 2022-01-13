62a06a3ecd4c0119bbf932496d6fe2646091b84e
Vladyslav Svitana <vladik719@gmail.com>
2022-01-13 19:36:56 +0100

    js-scripts-with-localStorage

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+// import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}
+
+const savedSettings = localStorage.getItem('videoplayer-current-time');
+const parsedSettings = JSON.parse(savedSettings);
+
+player.setCurrentTime(parsedSettings.seconds).then(function (seconds) {
+
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+var throttle = require('lodash.throttle');
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+//Checking the existing localeStorage for form's elements data
+const savedData = localStorage.getItem(STORAGE_KEY);
+const savedDataParsed = JSON.parse(savedData);
+// console.log(savedDataParsed);
+
+if (savedDataParsed) {
+    form['email'].value = savedDataParsed.email;
+    form['message'].value = savedDataParsed.message;
+}
+//Checking for input activity and setting localeStorage for formData object, which contains form's elements values
+form.addEventListener('input', throttle(onInput, 500)); //update localStorage every 500ms
+
+function onInput() {
+    const formData = { email: `${form["email"].value}`, message: `${form["message"].value}` };
+    const formDataSaved = JSON.stringify(formData);
+
+    localStorage.setItem(STORAGE_KEY, formDataSaved);
+}
+
+form.addEventListener('submit', onSubmit);
+//Cleaning localStorage and form fields on submit and returning object keys and values
+function onSubmit(evt) {
+    const formData = { email: `${form['email'].value}`, message: `${form['message'].value}` };
+    console.log(formData);
+
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
